3f1e379787c6fb0ab47f6d69446494e1726f2635
Juan <juanjiga@users.noreply.github.com>
2016-10-26 22:50:48 +0200

    Update MainActivity.java

--- app/src/main/java/com/juanjiga/hilos/MainActivity.java
+++ app/src/main/java/com/juanjiga/hilos/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
+    Button boton;
     TextView textView;
 
     @Override
