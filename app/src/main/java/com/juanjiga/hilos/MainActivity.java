e566c4d741c12ca74bf2ccd4ae2a229cf4cbaa5d
juanjiga <juanjiga71@hotmail.es>
2016-10-26 14:06:43 +0200

    recibido en taller y primer commit desde este

--- app/src/main/java/com/juanjiga/hilos/MainActivity.java
+++ app/src/main/java/com/juanjiga/hilos/MainActivity.java
@@ -2,12 +2,28 @@ package com.juanjiga.hilos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById((R.id.textView));
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
